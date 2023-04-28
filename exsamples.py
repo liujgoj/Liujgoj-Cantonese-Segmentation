f3abf035b81e0d36c9186bcd52739289860f35bf
Liujgoj Cantonese（溜歌粵語） <97525770+liujgoj@users.noreply.github.com>
2023-04-28 22:13:31 +0800

    Create exsamples.py

---
+++ exsamples.py
@@ -0,0 +1,23 @@
+"""
+Example sentences to test.
+"""
+
+
+sentences = [
+    "Ngoq oi neiq.", 
+    # 我愛你。
+    "Yamr caax.",
+    #  飲茶。
+    "Keiq hair douh!",
+    # 企喺度。
+    "Gwongrzauj.",
+    # 廣州。
+    "Heoi binj aa?",
+    # 去邊啊？
+    "Neiq wanr binj go?", 
+    # 你搵邊個？
+    "Yauq mouq gaaur co?",
+    # 有冇搞錯？
+    "Geirsix ceotjfaat?",
+    # 幾時出發？
+]
