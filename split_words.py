23d538e53123e98334b45d2cf9212faf3fb15bc7
Liujgoj Cantonese（溜歌粵語） <97525770+liujgoj@users.noreply.github.com>
2023-04-30 22:02:40 +0800

    Update split_words.py

--- split_words.py
+++ split_words.py
@@ -1,65 +1,24 @@
 import re
 
-def process_word(word):
-    word = word.lower()
-    
-    # 處理撇号
-    word = re.sub(r"([a-z]+)'", r"\1", word)
-    word = re.sub(r"'([a-z]+)", r"\1", word)
+# 定义前缀和后缀的正则表达式
+prefix_pattern = r'(?:aa|louq|daaih|sai|siur)-'
+suffix_pattern = r'-[a-z]*(?:saangj|yij|goj|goh|baak|sukj|taair|zair|neoiq|neoir|maaj|noengr|gaaj|gei|sih|yex|fur|zer|zej|gungj|ciux|satj|wongx|guj|samr|zikh|siu)'
 
-    # 處理動詞
-    word = re.sub(r"(.*)(ganr|zor|zvh|gwo)$", r"\1", word)
+# 读取文件内容
+with open('manxginr.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
 
-    # 處理連字符
-    if '-' in word:
-        # 特殊處理
-        if word == "mouq-zing-mouq-geoi":
-            return ["mouq", "zinggeoi"]
-        if word == "mouq-sih-mouq-gon":
-            return ["mouq", "sihgon"]
-        if word == "yauq-zungj-yauq-yih":
-            return ["yauq", "zungjyih"]
+# 去除前缀和后缀
+content = re.sub(prefix_pattern, '', content, flags=re.IGNORECASE)
+content = re.sub(suffix_pattern, '', content)
 
-        # 前綴
-        word = re.sub(r"^(aa-|louq-|daaih-|sai-|siur-)", "", word)
+# 将大写字母转为小写
+content = content.lower()
 
-        # 後綴
-        word = re.sub(r"(-saangj|-yij|-goj|-goh|-baak|-sukj|-taair|-zair|-neoiq|-neoir|-maaj|-noengr|-gaaj|-gei|-sih|-yex|-fur|-zer|-gungj|-ciux|-satj|-wongx|-guj|-samr|-zikh|-siu)$", "", word)
 
-        # 其他處理
-        parts = word.split('-')
-        n = len(parts)
+# 分割单词
+words = re.findall(r'[\W\S]+', content)
 
-        if n == 1:
-            return [parts[0]]
-
-        if parts[0] == parts[-1]:
-            return [parts[0]]
-        else:
-            if n == 2:
-                return [parts[0], parts[1]]
-            else:
-                return parts[:-1] + [parts[-1]]
-
-    return [word]
-
-def split_words(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        words = re.findall(r"[a-zA-Z'-]+", content)
-        words = [process_word(word) for word in words]
-
-    return words
-
-def write_words(words, filename):
-    with open(filename, 'w', encoding='utf-8') as file:
-        for word_list in words:
-            for word in word_list:
-                file.write(word + '\n')
-
-if __name__ == '__main__':
-    input_file = 'manxginr.txt'
-    output_file = 'daanjcix.txt'
-
-    words = split_words(input_file)
-    write_words(words, output_file)
+# 将单词写入文件
+with open('1.txt', 'w') as f:
+    f.write('\n'.join(words))
