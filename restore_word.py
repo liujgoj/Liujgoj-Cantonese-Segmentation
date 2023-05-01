54de4ebfcc45f00e1359cc79f6e8d27d9cfef9b6
Liujgoj Cantonese（溜歌粵語） <97525770+liujgoj@users.noreply.github.com>
2023-05-01 16:11:53 +0800

    Update restore_word.py

--- restore_word.py
+++ restore_word.py
@@ -1,5 +1,3 @@
-import re
-
 # 读取文件1.txt
 with open('1.txt', 'r', encoding='UTF-8') as f:
     content = f.read()
@@ -7,14 +5,14 @@ with open('1.txt', 'r', encoding='UTF-8') as f:
 # 将大写字母转为小写
 content = content.lower()
 
-# 替换指定字符串
+# 替换字符串
 content = content.replace("hor' mxhoryiq", "horyiq mxhoryiq")
 content = content.replace("hor' mxhornangx", "hornangx mxhornangx")
 content = content.replace("'deih", "keoiqdeih")
 
 # 划分单词
-words = re.findall(r"[^\s\d\W\-]+", content)
+words = content.split()
 
-# 将单词写入文件2.txt
+# 保存修改后的结果到文件2.txt
 with open('2.txt', 'w', encoding='UTF-8') as f:
-    f.write("\n".join(words))
+    f.write('\n'.join(words))
