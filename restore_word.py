da23c4455ce58401d649b649524b10391f0eb2d7
Liujgoj Cantonese（溜歌粵語） <97525770+liujgoj@users.noreply.github.com>
2023-05-01 13:57:08 +0800

    Update restore_word.py

--- restore_word.py
+++ restore_word.py
@@ -1,13 +1,11 @@
-with open("1.txt", "r", encoding="utf-8") as file1, open("2.txt", "w", encoding="utf-8") as file2:
-    for line in file1:
-        # Step 1: replace "hor' mxhoryiq" with "horyiq mxhoryiq"
-        line = line.replace("hor' mxhoryiq", "horyiq mxhoryiq")
-        
-        # Step 2: replace "hor' mxhornangx" with "hornangx mxhornangx"
+with open('1.txt', 'r', encoding='utf-8') as f1, open('2.txt', 'w', encoding='utf-8') as f2:
+    for line in f1:
+        line = line.lower()  # 把大写字母全部转为小写
+        line = line.replace("hor' mxhoryiq", "horyiq mxhoryiq")  # 替换字符串
         line = line.replace("hor' mxhornangx", "hornangx mxhornangx")
-        
-        # Step 3: replace "'deih" with "keoiqdeih"
         line = line.replace("'deih", "keoiqdeih")
-        
-        # Write the modified line to file2
-        file2.write(line)
+        words = line.split()  # 分割单词
+        for word in words:
+            f2.write(word)
+            f2.write('\n')  # 每个单词占一行
+            
